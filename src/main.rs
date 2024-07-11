f5d971941c10874711041fa642cb3d4ad2433eba
Christian Schwarz <christian@neon.tech>
2024-07-11 11:46:15 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,195 @@
+use core::panic;
+use std::{
+    collections::HashMap,
+    io::{Read, Seek, Write},
+    mem::MaybeUninit,
+    num::NonZeroUsize,
+    os::{
+        fd::AsRawFd,
+        unix::fs::{FileExt, OpenOptionsExt},
+    },
+    path::PathBuf,
+    sync::{atomic::AtomicBool, Arc},
+    time::{Duration, Instant},
+};
+
+use clap::Parser;
+use procfs::Current;
+use rand::{Rng, RngCore};
+
+#[derive(clap::Parser, Debug)]
+struct Args {
+    path: PathBuf,
+    size: byte_unit::Byte,
+    #[clap(long, default_value = "4Ki")]
+    io_size_and_align: byte_unit::Byte,
+    #[clap(long)]
+    direct: bool,
+    #[clap(long)]
+    fallocate: bool,
+    #[clap(long)]
+    dump_kstack_after_ctrlc: bool,
+}
+
+fn main() {
+    let args = dbg!(Args::parse());
+
+    let mut file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .read(true)
+        .custom_flags(if args.direct { libc::O_DIRECT } else { 0 })
+        .open(&args.path)
+        .unwrap();
+
+    if args.direct {
+        let mut mystatx: MaybeUninit<libc::statx> = MaybeUninit::uninit();
+        unsafe {
+            let err = libc::statx(
+                file.as_raw_fd(),
+                c"".as_ptr(),
+                libc::AT_EMPTY_PATH,
+                libc::STATX_DIOALIGN,
+                mystatx.as_mut_ptr(),
+            );
+            assert_eq!(err, 0);
+            let mystatx = mystatx.assume_init();
+            if mystatx.stx_mask & libc::STATX_DIOALIGN == 0 {
+                eprintln!("DIOALIGN not supported, cannot validate io_size");
+            } else {
+                dbg!(mystatx.stx_dio_mem_align);
+                dbg!(mystatx.stx_dio_offset_align);
+                let io_size_and_align = args.io_size_and_align.as_u64();
+                assert_eq!(io_size_and_align % (mystatx.stx_dio_mem_align as u64), 0);
+                assert_eq!(io_size_and_align % (mystatx.stx_dio_offset_align as u64), 0);
+            }
+        };
+    };
+    let make_buffer = || {
+        let mapping = unsafe {
+            nix::sys::mman::mmap_anonymous(
+                None,
+                NonZeroUsize::new(args.io_size_and_align.as_u64() as usize).unwrap(),
+                nix::sys::mman::ProtFlags::PROT_READ | nix::sys::mman::ProtFlags::PROT_WRITE,
+                nix::sys::mman::MapFlags::MAP_PRIVATE,
+            )
+            .unwrap()
+        };
+        assert_eq!(
+            (mapping.as_ptr() as usize) % args.io_size_and_align.as_u64() as usize,
+            0
+        );
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                mapping.as_ptr() as *mut u8,
+                args.io_size_and_align.as_u64() as usize,
+            )
+        }
+    };
+
+    file.set_len(args.size.as_u64()).unwrap();
+
+    if args.fallocate {
+        nix::fcntl::fallocate(
+            file.as_raw_fd(),
+            nix::fcntl::FallocateFlags::FALLOC_FL_ZERO_RANGE,
+            0,
+            args.size.as_u64() as i64,
+        )
+        .unwrap();
+    }
+
+    println!("Initialized");
+
+    std::thread::spawn(|| {
+        let myself = procfs::process::Process::myself().unwrap();
+        loop {
+            dump_memory_metrics(&myself);
+            std::thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
+
+    let stop = Arc::new(AtomicBool::new(false));
+
+    ctrlc::set_handler({
+        let stop = Arc::clone(&stop);
+        move || {
+            println!("Signalling stop");
+            stop.store(true, std::sync::atomic::Ordering::Relaxed);
+            let myself = procfs::process::Process::myself().unwrap();
+            if !args.dump_kstack_after_ctrlc {
+                return;
+            }
+            if myself.uid().unwrap() != 0 {
+                println!("Not root, skipping stack dump");
+                return;
+            }
+            std::thread::spawn(move || {
+                let mut buf = String::new();
+                loop {
+                    let mut res = myself.open_relative("stack").unwrap();
+                    buf.clear();
+                    res.read_to_string(&mut buf).unwrap();
+                    println!("{}", buf);
+                    std::thread::sleep(Duration::from_secs(1));
+                }
+            });
+        }
+    })
+    .unwrap();
+
+    // indefinitly write blobs of size and alignment args.io_size_and_align into the file
+    let mut write_buf = make_buffer();
+    let mut read_buf = make_buffer();
+    while !stop.load(std::sync::atomic::Ordering::Relaxed) {
+        let offset = rand::thread_rng()
+            .gen_range(0..args.size.as_u64() / (args.io_size_and_align.as_u64()))
+            * (args.io_size_and_align.as_u64());
+        if rand::thread_rng().gen_bool(1.0) {
+            rand::thread_rng().fill_bytes(&mut write_buf);
+            if let Err(e) = file.write_all_at(&write_buf, offset) {
+                panic!("write failed: {offset:x} {e}");
+            }
+        } else {
+            file.read_exact_at(&mut read_buf, offset).unwrap();
+        }
+    }
+    println!("writer stopped, closing file descriptor");
+    drop(file);
+    println!("file descriptor closed, exiting");
+}
+
+fn dump_memory_metrics(myself: &procfs::process::Process) {
+    let page_sz = procfs::current_system_info().page_size();
+    let vmstat = procfs::vmstat().unwrap();
+    let smaps_rollup = {
+        // https://www.kernel.org/doc/Documentation/ABI/testing/procfs-smaps_rollup
+        let rollup = myself.smaps_rollup().unwrap();
+        let cum: Vec<_> = rollup.memory_map_rollup.into_iter().collect();
+        assert_eq!(cum.len(), 1);
+        let rollup: procfs::process::MemoryMap = cum.into_iter().next().unwrap();
+        let keys = ["Pss_Anon", "Pss_File", "Pss_Shmem"];
+        let out: Vec<_> = keys
+            .into_iter()
+            .map(|key| (key, rollup.extension.map[key]))
+            .collect();
+        out
+    };
+    let procfs::process::Status {
+        vmrss,
+        rssanon,
+        rssfile,
+        rssshmem,
+        vmsize,
+        ..
+    } = myself.status().unwrap();
+    println!(
+        "process: vmrss: {:?}, rssanon: {:?}, rssfile: {:?}, rssshmem: {:?}, vmsize: {:?}, smaps_rollup: {:?}",
+        vmrss, rssanon, rssfile, rssshmem, vmsize, smaps_rollup,
+    );
+    println!(
+        "system : nr_dirty: {} MiB",
+        (u64::try_from(vmstat["nr_dirty"]).unwrap() * page_sz) >> 20
+    );
+}
